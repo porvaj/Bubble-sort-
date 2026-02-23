f916743622a38dc1f249abcf540b9fbd2ca7c8c3
porvaj <porvaj22@gmail.com>
2026-02-23 08:18:13 +0530

    Add files via upload

---
+++ linear search.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target)
+            return i;   // Return index if found
+    }
+    return -1;          // Return -1 if not found
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+
+    int result = linearSearch(arr, n, target);
+
+    if(result != -1)
+        cout << "Element found at index: " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
