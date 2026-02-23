697924d06f6fd675bc3813d8446998521d299eda
porvaj <porvaj22@gmail.com>
2026-02-23 08:15:38 +0530

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while(left <= right) {
+        int mid = left + (right - left) / 2;  // Avoid overflow
+
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;  // Element not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+
+    int result = binarySearch(arr, n, target);
+
+    if(result != -1)
+        cout << "Element found at index: " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
